3636973e3ceb53a3f520d642d4d08bd0971d56be
JJJJungw <152381600+JJJJungw@users.noreply.github.com>
2025-07-02 21:45:31 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        String[] mes = new String[message.length()];
+        answer = mes.length * 2;
+        return answer;
+    }
+}
