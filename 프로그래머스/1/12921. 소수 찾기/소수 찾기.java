7bc8260d46571f6b626af97bf7d6b8a88f9d43ba
Amicitia <152381600+JJJJungw@users.noreply.github.com>
2025-08-14 01:50:05 +0900

    [level 1] Title: 소수 찾기, Time: 181.57 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -10,13 +10,12 @@ class Solution {
         return count;
     }
 
-    // 제곱근까지만 검사하도록 수정
     public boolean isPrime(int num) {
-        for (int i = 2; i * i <= num; i++) {  // i <= √num
+        for (int i = 2; i * i <= num; i++) {
             if (num % i == 0) {
                 return false;
             }
         }
-        return true; // 끝까지 나눠지지 않으면 소수
+        return true;
     }
 }
