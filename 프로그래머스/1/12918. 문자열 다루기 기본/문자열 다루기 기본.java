f5aaf3878afa5ea36b41908d52e8f97886df5d08
Amicitia <152381600+JJJJungw@users.noreply.github.com>
2025-08-26 21:44:17 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.08 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean solution(String s) {
+        // 길이가 4 또는 6인지 확인
+        if (s.length() != 4 && s.length() != 6) {
+            return false;
+        }
+        
+        // 모든 문자가 숫자인지 확인
+        for (int i = 0; i < s.length(); i++) {
+            if (!Character.isDigit(s.charAt(i))) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
