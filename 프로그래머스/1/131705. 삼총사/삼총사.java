29db987c2f09290eb306295c219b408d839495a4
Amicitia <152381600+JJJJungw@users.noreply.github.com>
2026-01-11 17:56:24 +0900

    [level 1] Title: 삼총사, Time: 0.56 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,19 @@
+class Solution {
+    int count = 0;
+
+    public int solution(int[] number) {
+        dfs(number, 0, 0, 0);
+        return count;
+    }
+
+    private void dfs(int[] number, int start, int depth, int sum) {
+        if (depth == 3) {
+            if (sum == 0) count++;
+            return;
+        }
+
+        for (int i = start; i < number.length; i++) {
+            dfs(number, i + 1, depth + 1, sum + number[i]);
+        }
+    }
+}
