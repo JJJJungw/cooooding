ac0c94e9530ea393f6d9dc86a1f88bfeb879ab08
Amicitia <152381600+JJJJungw@users.noreply.github.com>
2026-01-12 16:33:20 +0900

    [level 1] Title: 최소직사각형, Time: 2.13 ms, Memory: 93 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int maxw = 0;
+        int maxh = 0;
+        for(int card[] : sizes){
+            int w = Math.max(card[0],card[1]);
+            int h = Math.min(card[0],card[1]);
+            maxw = Math.max(maxw,w);
+            maxh = Math.max(maxh,h);
+        }
+        return maxw*maxh;
+    }
+}
