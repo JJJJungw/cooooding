f891cb3e61ba5d2760d54b2df985086c941fb8fd
Amicitia <152381600+JJJJungw@users.noreply.github.com>
2026-01-17 18:32:32 +0900

    [level 1] Title: 덧칠하기, Time: 1.83 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,22 +1,15 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int[] wall = new int[n];
+        int count = 0;
+        int end = 0; // 마지막으로 칠해진 위치
 
-        for (int i = 0; i < section.length; i++) {
-            wall[section[i] - 1] = 1;
-        }
-
-        int cnt = 0;
-         for (int i = 0; i < n; i++) {
-            if (wall[i] == 1) {
-            cnt++;
-            for (int j = 0; j < m && i + j < n; j++) {
-                wall[i + j] = 0;
-                }
-            i = i + m - 1;
+        for (int s : section) {
+            // 아직 이 구역이 칠해지지 않았다면
+            if (s > end) {
+                count++;
+                end = s + m - 1; // 롤러로 덮는 끝 지점
             }
-         }
-        
-        return cnt;
+        }
+        return count;
     }
 }
