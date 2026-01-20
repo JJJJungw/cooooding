8e5788b2b0fdd6250544480860975991c072d4b2
Amicitia <152381600+JJJJungw@users.noreply.github.com>
2026-01-20 21:03:27 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.09 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+
+        int count = 0;      
+        int undefined = 0; 
+
+        Set<Integer> set = new HashSet<>();
+        for (int i : win_nums) {
+            set.add(i);
+        }
+
+        for (int i = 0; i < lottos.length; i++) {
+            if (lottos[i] == 0) {
+                undefined++;
+            } else if (set.contains(lottos[i])) {
+                count++;
+            }
+        }
+
+        int high = count + undefined;
+        int low = count;
+
+        answer[0] = rank(high);
+        answer[1] = rank(low);
+
+        return answer;
+    }
+
+    private int rank(int match) {
+        switch (match) {
+            case 6: return 1;
+            case 5: return 2;
+            case 4: return 3;
+            case 3: return 4;
+            case 2: return 5;
+            default: return 6;
+        }
+    }
+}
