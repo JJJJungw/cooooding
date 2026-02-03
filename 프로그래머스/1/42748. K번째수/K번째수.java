e8b5d747eaaebae31d3cd54a2a77651675161ea6
Amicitia <152381600+JJJJungw@users.noreply.github.com>
2026-02-03 20:54:52 +0900

    [level 1] Title: K번째수, Time: 0.36 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,16 +2,21 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        for (int i = 0; i < commands.length; i++) {
-            int start = commands[i][0];
-            int end   = commands[i][1];
-            int k     = commands[i][2];
+        
+        for(int i = 0; i<commands.length; i++){
+            int start = commands[i][0]; //2 2번째 부터 잘라라(인덱스가 아님 0번부터 시작하는게 아님 1번째 인덱스부터)
+            int end = commands[i][1]; //5 (5번째 즉 4번째 인덱스)
+            int idx = commands[i][2]; //3
+            int len = end - start +1;
             
-            int[] cut = Arrays.copyOfRange(array,start-1,end);
+            int[] cut = new int[len];
+            for(int j = 0; j<len; j++){
+                cut[j]=array[start-1+j];
+            }
             Arrays.sort(cut);
-            answer[i] = cut[k-1];
-            
+            answer[i]=cut[idx-1];
         }
+        
         return answer;
     }
 }
